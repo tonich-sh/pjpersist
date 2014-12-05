dfcdf0e8b520890d006fdcde9ffb47143cad5451
Albertas Agejevas <alga@pov.lt>
2014-12-06 00:09:19 +0200

    Added PG array support.

--- src/pjpersist/sqlbuilder.py
+++ src/pjpersist/sqlbuilder.py
@@ -22,6 +22,36 @@ from sqlobject.sqlbuilder import *
 # Let's have them here and then see if they're worth contributing
 # upstream when they settle down.
 
+class PGArray(SQLExpression):
+    """PostgreSQL array expression.
+
+    The downside of current implementation is that the items must be
+    the same type.  Literal arrays like '{1, 1.5, "two"}' can be
+    non-homogenous, but need different quoting of strings.
+    """
+
+    def __init__(self, iterable):
+        self.iterable = iterable
+
+    def __sqlrepr__(self, db):
+        assert db == 'postgres', "Postgres-specific feature, sorry."
+
+        items = (sqlrepr(item, db) for item in self.iterable)
+        return 'array[%s]' % ", ".join(items)
+
+
+class JSONB(SQLExpression):
+    """Cast to JSONB"""
+
+    cast = "::jsonb"
+
+    def __init__(self, arg):
+        self.arg = arg
+
+    def __sqlrepr__(self, db):
+        return sqlrepr(self.arg, db) + self.cast
+
+
 def JSON_GETITEM(json, key):
     return SQLOp("->", json, key)
 
@@ -30,11 +60,11 @@ def JSON_GETITEM_TEXT(json, key):
 
 def JSON_PATH(json, keys):
     """keys is an SQL array"""
-    return SQLOp("#>", json, key)
+    return SQLOp("#>", json, PGArray(keys))
 
 def JSON_PATH_TEXT(json, keys):
     """keys is an SQL array"""
-    return SQLOp("#>>", json, key)
+    return SQLOp("#>>", json, PGArray(keys))
 
 def JSONB_SUPERSET(superset, subset):
     return SQLOp("@>", superset, subset)
@@ -47,11 +77,8 @@ def JSONB_CONTAINS(jsonb, key):
 
 def JSONB_CONTAINS_ANY(jsonb, keys):
     """keys is an SQL array"""
-    return SQLOp("?|", jsonb, keys)
+    return SQLOp("?|", jsonb, PGArray(keys))
 
 def JSONB_CONTAINS_ALL(jsonb, keys):
     """keys is an SQL array"""
-    return SQLOp("?&", jsonb, keys)
-
-
-
+    return SQLOp("?&", jsonb, PGArray(keys))
