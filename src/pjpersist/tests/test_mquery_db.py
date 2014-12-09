b41bca443565226d2fb74571a6fb3dfd7b27c503
Albertas Agejevas <alga@pov.lt>
2014-12-10 01:59:54 +0200

    More text in doctest.

--- src/pjpersist/tests/test_mquery_db.py
+++ src/pjpersist/tests/test_mquery_db.py
@@ -76,9 +76,13 @@ def doctest_operators():
        >>> select(conn, {'some.numbers': 69})
        {u'some': {u'numbers': [42, 69, 105]}}
 
+    Comparison operators:
+
        >>> select(conn, {'nr': {'$gt': 40}})
        {u'nr': 42}
        {u'nr': 42, u'drink': u'whiskey', u'day': u'Friday', u'plan': u'getdown'}
+
+    List searches:
 
        >>> select(conn, {'nr': {'$in': [40, 41, 42]}})
        {u'nr': 42}
@@ -90,12 +94,16 @@ def doctest_operators():
        >>> select(conn, {'foo': {'$nin': ['foo', 'baz']}})
        {u'foo': u'bar'}
 
+    Existence of keys:
+
        >>> select(conn, {'some.numbers': {'$exists': True}})
        {u'some': {u'numbers': [42, 69, 105]}}
 
        >>> select(conn, {'nr': {'$exists': False}})
        {u'foo': u'bar'}
        {u'some': {u'numbers': [42, 69, 105]}}
+
+    Negation of comparisons:
 
        >>> select(conn, {'nr': {'$not': {'$gt': 40}}})
        {u'foo': u'bar'}
@@ -107,8 +115,12 @@ def doctest_operators():
        {u'some': {u'numbers': [42, 69, 105]}}
        {u'nr': 42, u'drink': u'whiskey', u'day': u'Friday', u'plan': u'getdown'}
 
+    List sizes:
+
        >>> select(conn, {'some.numbers': {'$size': 3}})
        {u'some': {u'numbers': [42, 69, 105]}}
+
+    $all:
 
        >>> select(conn, {'some.numbers': {'$all': [69, 42]}})
        {u'some': {u'numbers': [42, 69, 105]}}
